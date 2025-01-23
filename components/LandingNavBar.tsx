5e01571e93b86a05bcd6d39ea72d67339a4dac42
Amitesh Singh <amiteshsingh252@gmail.com>
2025-01-23 20:12:44 +0530

    commit

--- components/LandingNavBar.tsx
+++ components/LandingNavBar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const LandingNavBar = async () => {
   return (
     <div className="dark absolute top-0 w-full p-5 sm:px-10 md:px-20 flex justify-between items-center">
-      <h1 className="text-2xl font-bold">AI Platform</h1>
+      <h1 className="text-2xl font-bold">Interactive Chat Bot</h1>
       <Link href="/auth">
         <Button className="bg-secondary-btn text-secondary-btn-text" >Get Started</Button>
       </Link>
